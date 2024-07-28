44e785fd1c45034296825b0716216de97fc2f0f3
Shota Nozaki <emonkak@gmail.com>
2024-07-29 01:00:09 +0900

    feat: suppress "'event' is deprecated." warning

---
+++ src/typings/deprecatedEvent.d.ts
@@ -0,0 +1,8 @@
+interface Window {
+  /**
+   * This property is marked as deprecated. But we use this to determine the
+   * task priority. This definition suppresses "'event' is deprecated." warning
+   * reported by VSCode.
+   */
+  readonly event: Event | undefined;
+}
