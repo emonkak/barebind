2282727ed4018b574c43a77682200a889a3f2c64
Shota Nozaki <emonkak@gmail.com>
2025-07-25 21:54:29 +0900

    feat(extensions.observable): integrate Observable class

--- src/extensions/observable.ts
+++ src/extensions/observable.ts
@@ -6,212 +6,207 @@ import {
   type Subscription,
 } from './signal.js';
 
-type Observable<T> = T extends object
-  ? T extends readonly (infer Element)[]
-    ? ObservableArray<Element>
-    : ObservableObject<T>
-  : Atom<T>;
+const TYPE_ARRAY = 0;
+const TYPE_OBJECT = 1;
+const TYPE_PRIMITIVE = 2;
 
-type ObservableProperty<T, K extends keyof T> = IsWritable<T, K> extends true
-  ? Observable<T[K]>
-  : Computed<T[K]>;
-
-type ObservableKeys<T> = Exclude<
-  Extract<keyof T, string>,
-  PrivateKeys | FunctionKeys<T>
->;
-
-type PrivateKeys = `_${string}`;
-
-type FunctionKeys<T> = {
-  [K in keyof T]: T[K] extends Function ? K : never;
-}[keyof T];
-
-type IsWritable<T, K extends keyof T> = StrictEqual<
-  { -readonly [P in K]-?: T[P] },
-  Pick<T, K>
->;
-
-type StrictEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
-  T,
->() => T extends Y ? 1 : 2
-  ? true
-  : false;
-
-export class ObservableArray<T> extends Signal<readonly T[]> {
-  private readonly _observableElements: Atom<(Signal<T> | undefined)[]>;
-
-  private _memoizedSource: readonly T[];
-
-  private _memoizedVersion: number;
-
-  constructor(source: readonly T[]) {
-    super();
-
-    this._observableElements = new Atom(new Array(source.length));
-    this._memoizedSource = source;
-    this._memoizedVersion = 0;
-  }
-
-  get value(): readonly T[] {
-    const currentVersion = this.version;
-
-    if (this._memoizedVersion < currentVersion) {
-      const observableElements = this._observableElements.value;
-      const source = this._memoizedSource.slice();
-
-      for (let i = 0, l = source.length; i < l; i++) {
-        const observableElement = observableElements[i];
-        if (observableElement !== undefined) {
-          source[i] = observableElement.value as T;
-        }
-      }
-
-      this._memoizedSource = source;
-      this._memoizedVersion = currentVersion;
+type ObservableDescriptor<T> =
+  | {
+      type: typeof TYPE_ARRAY;
+      source: T & readonly unknown[];
+      memoizedVersion: number;
+      observableElements: Signal<unknown>[];
     }
-
-    return this._memoizedSource;
-  }
-
-  set value(source: readonly T[]) {
-    this._observableElements.value = new Array(source.length);
-    this._memoizedSource = source;
-    this._memoizedVersion = 0;
-  }
-
-  get version(): number {
-    const observableElements = this._observableElements.value;
-    let version = 0;
-
-    for (let i = 0, l = observableElements.length; i < l; i++) {
-      version += observableElements[i]?.version ?? 0;
+  | {
+      type: typeof TYPE_OBJECT;
+      source: T & object;
+      memoizedVersion: number;
+      observableProperties: Map<PropertyKey, Signal<unknown>>;
     }
+  | {
+      type: typeof TYPE_PRIMITIVE;
+      source: T;
+    };
 
-    return version;
+export namespace Observable {
+  export type Value<T> = T extends object
+    ? T extends readonly (infer Element)[]
+      ? Array<Element>
+      : T extends Function
+        ? Primitive<T>
+        : Object<T>
+    : Primitive<T>;
+
+  export interface Array<T> extends Signal<readonly T[]> {
+    readonly length: number;
+    value: readonly T[];
+    get(index: number): Value<T> | undefined;
   }
 
-  get length(): number {
-    return this._memoizedSource.length;
+  export interface Object<T extends object> extends Signal<T> {
+    value: T;
+    get<K extends ObservableKeys<T>>(key: K): ObservableProperty<T, K>;
   }
 
-  get(index: number): Observable<T> | undefined {
-    const observableElements = this._observableElements.value;
-    let element$ = observableElements[index];
-
-    if (element$ === undefined) {
-      element$ = toObservable(this._memoizedSource[index]!);
-
-      element$.subscribe(() => {
-        this._observableElements.notifySubscribers();
-      });
-
-      observableElements[index] = element$;
-    }
-
-    return element$ as Observable<T> | undefined;
+  export interface Primitive<T> extends Signal<T> {
+    value: T;
   }
 
-  set(index: number, value: T): void {
-    const observableElements = this._observableElements.value;
-    const element$ = toObservable(value);
+  type ObservableProperty<T, K extends keyof T> = IsWritable<T, K> extends true
+    ? Value<T[K]>
+    : Computed<T[K]>;
 
-    observableElements[index] = element$;
+  type ObservableKeys<T> = Exclude<
+    Extract<AllKeys<T>, string>,
+    FunctionKeys<T> | PrivateKeys
+  >;
 
-    this._memoizedVersion = -1;
-  }
+  type AllKeys<T> = T extends any ? keyof T : never;
 
-  subscribe(subscriber: Subscriber): Subscription {
-    return this._observableElements.subscribe(subscriber);
-  }
+  type FunctionKeys<T> = {
+    [K in AllKeys<T>]: T[K] extends Function ? K : never;
+  }[AllKeys<T>];
+
+  type PrivateKeys = `_${string}`;
+
+  type IsWritable<T, K extends keyof T> = StrictEqual<
+    { -readonly [P in K]-?: T[P] },
+    Pick<T, K>
+  >;
+
+  type StrictEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+    T,
+  >() => T extends Y ? 1 : 2
+    ? true
+    : false;
 }
 
-export class ObservableObject<T extends object> extends Signal<T> {
-  private readonly _observableProperties: Atom<
-    Map<PropertyKey, Signal<unknown>>
-  > = new Atom(new Map());
+export class Observable<T> extends Signal<T> {
+  private _descriptor$: Atom<ObservableDescriptor<T>>;
 
-  private _memoizedSource: T;
-
-  private _memoizedVersion: number;
+  static from<T>(source: T): Observable.Value<T> {
+    return new Observable(source) as any;
+  }
 
   constructor(source: T) {
     super();
+    this._descriptor$ = new Atom(toObservableDescriptor(source));
+  }
 
-    this._memoizedSource = source;
-    this._memoizedVersion = 0;
+  get length(): number {
+    const descriptor = this._descriptor$.value;
+    return descriptor.type === TYPE_ARRAY ? descriptor.source.length : 0;
   }
 
   get value(): T {
-    const currentVersion = this.version;
+    const descriptor = this._descriptor$.value;
 
-    if (this._memoizedVersion < currentVersion) {
-      const observableProperties = this._observableProperties.value;
-      const source = cloneObject(this._memoizedSource);
+    switch (descriptor.type) {
+      case TYPE_ARRAY: {
+        const { observableElements, memoizedVersion } = descriptor;
+        const currentVersion = this._descriptor$.version;
 
-      for (const [key, observableProperty] of observableProperties.entries()) {
-        source[key as keyof T] = observableProperty.value as T[keyof T];
+        if (memoizedVersion < currentVersion) {
+          const source = descriptor.source.slice() as T & unknown[];
+
+          for (let i = 0, l = source.length; i < l; i++) {
+            const observableElement = observableElements[i];
+            if (observableElement !== undefined) {
+              source[i] = observableElement.value;
+            }
+          }
+
+          descriptor.source = Object.freeze(source);
+          descriptor.memoizedVersion = currentVersion;
+        }
+        break;
       }
+      case TYPE_OBJECT: {
+        const { observableProperties, memoizedVersion } = descriptor;
+        const currentVersion = this._descriptor$.version;
 
-      this._memoizedSource = source;
-      this._memoizedVersion = currentVersion;
+        if (memoizedVersion < currentVersion) {
+          const source = cloneObject(descriptor.source);
+
+          for (const [key, property$] of observableProperties.entries()) {
+            if (property$ instanceof Observable) {
+              source[key as keyof T] = property$.value as (T & object)[keyof T];
+            }
+          }
+
+          descriptor.source = Object.freeze(source);
+          descriptor.memoizedVersion = currentVersion;
+        }
+        break;
+      }
     }
 
-    return this._memoizedSource;
+    return descriptor.source;
   }
 
-  set value(source: T) {
-    this._observableProperties.value = new Map();
-    this._memoizedSource = source;
-    this._memoizedVersion = 0;
+  set value(value: T) {
+    this._descriptor$.value = toObservableDescriptor(value);
   }
 
   get version(): number {
-    let version = 0;
-
-    for (const observableProperty of this._observableProperties.value.values()) {
-      version += observableProperty.version;
-    }
-
-    return version;
+    return this._descriptor$.version;
   }
 
-  get<TKey extends ObservableKeys<T>>(key: TKey): ObservableProperty<T, TKey>;
-  get(key: PropertyKey): Signal<unknown> | undefined;
   get(key: PropertyKey): Signal<unknown> | undefined {
-    const observableProperties = this._observableProperties.value;
-    let property$ = observableProperties.get(key);
+    const descriptor = this._descriptor$.value;
+    switch (descriptor.type) {
+      case TYPE_ARRAY: {
+        if (typeof key !== 'number') {
+          break;
+        }
 
-    if (
-      property$ === undefined &&
-      typeof key === 'string' &&
-      !key.startsWith('_')
-    ) {
-      property$ = toObservableProperty(this, this._memoizedSource, key);
+        const { observableElements } = descriptor;
+        let element$ = observableElements[key];
 
-      if (isObservable(property$)) {
-        property$.subscribe(() => {
-          this._observableProperties.notifySubscribers();
-        });
+        if (element$ === undefined) {
+          element$ = new Observable(descriptor.source[key]!);
+          element$.subscribe(() => {
+            this._descriptor$.touch();
+          });
+          observableElements[key] = element$;
+        }
 
-        observableProperties.set(key, property$);
+        return element$;
+      }
+      case TYPE_OBJECT: {
+        if (typeof key !== 'string' || key.startsWith('_')) {
+          break;
+        }
+
+        const { observableProperties } = descriptor;
+        let property$ = observableProperties.get(key);
+
+        if (property$ === undefined) {
+          property$ = createObservableProperty(
+            this as Observable<object>,
+            descriptor.source,
+            key,
+          );
+
+          if (property$ !== undefined) {
+            if (property$ instanceof Observable) {
+              property$.subscribe(() => {
+                this._descriptor$.touch();
+              });
+            }
+            observableProperties.set(key, property$);
+          }
+        }
+
+        return property$;
       }
     }
 
-    return property$;
-  }
-
-  set<TKey extends ObservableKeys<T>>(key: TKey, value: T[TKey]): void {
-    const observableProperties = this._observableProperties.value;
-    const property$ = toObservable(value);
-
-    observableProperties.set(key, property$);
-
-    this._memoizedVersion = -1;
+    return undefined;
   }
 
   subscribe(subscriber: Subscriber): Subscription {
-    return this._observableProperties.subscribe(subscriber);
+    return this._descriptor$.subscribe(subscriber);
   }
 }
 
@@ -219,38 +214,20 @@ function cloneObject<T extends object>(object: T): T {
   return Object.assign(Object.create(Object.getPrototypeOf(object)), object);
 }
 
-function isObservable<T>(value: T): value is T & Observable<T> {
-  return (
-    value instanceof Atom ||
-    value instanceof ObservableArray ||
-    value instanceof ObservableObject
-  );
-}
-
-function toObservable<T>(value: T): Signal<T> {
-  if (typeof value === 'object' && value !== null) {
-    return Array.isArray(value)
-      ? (new ObservableArray(value) as any)
-      : new ObservableObject(value);
-  } else {
-    return new Atom(value);
-  }
-}
-
-function toObservableProperty<T extends object>(
-  observable: ObservableObject<T>,
+function createObservableProperty<T extends object>(
+  observable: Observable<T>,
   source: T,
   key: PropertyKey,
 ): Signal<unknown> | undefined {
+  let prototype = source;
+
   do {
-    const descriptor = Object.getOwnPropertyDescriptor(source, key);
+    const descriptor = Object.getOwnPropertyDescriptor(prototype, key);
 
     if (descriptor !== undefined) {
-      const { enumerable, writable, get, value } = descriptor;
+      const { enumerable, get, value } = descriptor;
 
-      if (enumerable && writable) {
-        return toObservable(value);
-      } else if (get !== undefined) {
+      if (get !== undefined) {
         const dependencies: Signal<unknown>[] = [];
         const tracker = trackObservable(observable, source, dependencies);
         const initialResult = get.call(tracker);
@@ -264,55 +241,56 @@ function toObservableProperty<T extends object>(
           initialResult,
           initialVersion,
         );
+      } else if (enumerable) {
+        return new Observable(value);
       }
     }
 
-    source = Object.getPrototypeOf(source);
-  } while (source !== null && source !== Object.prototype);
+    prototype = Object.getPrototypeOf(prototype);
+  } while (prototype !== null && prototype !== Object.prototype);
 
   return undefined;
 }
 
+function toObservableDescriptor<T>(source: T): ObservableDescriptor<T> {
+  if (typeof source === 'object' && source !== null) {
+    if (Array.isArray(source)) {
+      return {
+        type: TYPE_ARRAY,
+        source: Object.freeze(source),
+        memoizedVersion: 0,
+        observableElements: new Array(source.length),
+      };
+    } else {
+      return {
+        type: TYPE_OBJECT,
+        source: Object.freeze(source),
+        memoizedVersion: 0,
+        observableProperties: new Map(),
+      };
+    }
+  } else {
+    return {
+      type: TYPE_PRIMITIVE,
+      source,
+    };
+  }
+}
+
 function trackObservable<T extends object>(
-  observale: ObservableObject<T>,
+  observale: Observable<T>,
   source: T,
   dependencies: Signal<unknown>[],
 ): T {
   return new Proxy(source, {
     get: (target, key, receiver) => {
-      const observaleProperty = observale.get(key);
-      if (observaleProperty !== undefined) {
-        dependencies.push(observaleProperty);
-        return observaleProperty.value;
+      const property$ = observale.get(key);
+      if (property$ !== undefined) {
+        dependencies.push(property$);
+        return property$.value;
       } else {
         return Reflect.get(target, key, receiver);
       }
     },
   });
 }
-
-class Counter {
-  count: number;
-
-  constructor(initialCount: number = 0) {
-    this.count = initialCount;
-  }
-
-  get doublyCount(): number {
-    return this.count * 2;
-  }
-}
-
-const source = new Counter(100);
-const counter = new ObservableObject(source);
-
-counter.subscribe(() => {
-  console.log('counter changed');
-});
-
-// console.log(counter.get('count').value);
-console.log(counter.get('doublyCount').value);
-
-counter.get('count').value++;
-
-console.log(counter.value);
