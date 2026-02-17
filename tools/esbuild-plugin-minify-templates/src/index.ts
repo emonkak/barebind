52ba29fe644774bd09853a318ed338cc7a38d950
Shota Nozaki <emonkak@gmail.com>
2026-02-17 23:41:49 +0900

    feat(tools.esbuild-plugin-minify-templates): add `css` to default template tag names

--- tools/esbuild-plugin-minify-templates/src/index.ts
+++ tools/esbuild-plugin-minify-templates/src/index.ts
@@ -13,7 +13,7 @@ const FILTER_PATTERN = /\.(jsx?|tsx?)$/;
 export function minifyTemplates(
   options: MinifyTemplateOptions = {},
 ): esbuild.Plugin {
-  const { tagNames = ['html', 'math', 'svg'] } = options;
+  const { tagNames = ['css', 'html', 'math', 'svg'] } = options;
 
   return {
     name: 'minify-templates',
