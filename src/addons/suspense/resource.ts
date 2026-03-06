1555ec45e330b0be455106560d9c6e58a7917823
Shota Nozaki <emonkak@gmail.com>
2026-03-07 00:09:12 +0900

    refactor(addons.suspense): rename `fetchResource` parameter to `fetcher` in Resource hook

--- src/addons/suspense/resource.ts
+++ src/addons/suspense/resource.ts
@@ -2,13 +2,13 @@ import type { HookFunction } from '../../core.js';
 import { Suspend } from './suspend.js';
 
 export const Resource = function Resource<T>(
-  fetchResource: (signal: AbortSignal) => Promise<T>,
+  fetcher: (signal: AbortSignal) => Promise<T>,
   dependencies: unknown[] = [],
 ): HookFunction<Suspend<T>> {
   return (context) => {
     const suspend = context.useMemo(() => {
       const controller = new AbortController();
-      const promise = fetchResource(controller.signal);
+      const promise = fetcher(controller.signal);
       return Suspend.await(promise, controller);
     }, dependencies);
 
