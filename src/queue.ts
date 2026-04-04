66468c7f78db6f450e4081760bd3a94131cb2485
Shota Nozaki <emonkak@gmail.com>
2026-04-04 16:12:12 +0900

    refactor(queue): replace linked list with array-based implementation

--- src/queue.ts
+++ src/queue.ts
@@ -1,47 +1,35 @@
-interface Item<T> {
-  value: T;
-  next: Item<T> | null;
-}
-
 export class Queue<T> {
-  private _head: Item<T> | null = null;
-  private _tail: Item<T> | null = null;
+  private readonly _items: (T | undefined)[] = [];
+  private _head: number = 0;
 
   *[Symbol.iterator](): Generator<T> {
-    for (let item = this._head; item !== null; item = item.next) {
-      yield item.value;
-      if (item === this._tail) {
-        break;
-      }
+    for (let i = this._head, l = this._items.length; i < l; i++) {
+      yield this._items[i]!;
     }
-  }
-
-  enqueue(value: T): void {
-    const item: Item<T> = {
-      value: value,
-      next: null,
-    };
-    if (this._tail !== null) {
-      this._tail.next = item;
-    } else {
-      this._head = item;
-    }
-    this._tail = item;
   }
 
   dequeue(): T | undefined {
-    if (this._head === null) {
+    if (this._items.length === 0) {
       return undefined;
     }
-    const { value, next } = this._head;
-    this._head = next;
-    if (next === null) {
-      this._tail = null;
+
+    const item = this._items[this._head];
+    this._items[this._head] = undefined;
+    this._head++;
+
+    if (this._head === this._items.length) {
+      this._items.length = 0;
+      this._head = 0;
     }
-    return value;
+
+    return item;
+  }
+
+  enqueue(item: T): void {
+    this._items.push(item);
   }
 
   peek(): T | undefined {
-    return this._head?.value;
+    return this._items[this._head];
   }
 }
