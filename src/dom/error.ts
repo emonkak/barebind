b643558c8f3b1c55278b2fa1cd0c0056c99a1d7b
Shota Nozaki <emonkak@gmail.com>
2026-06-04 22:52:36 +0900

    refactor: remove `node` and `view` properties from errors

--- src/dom/error.ts
+++ src/dom/error.ts
@@ -1,13 +1,10 @@
 import { generateNodeFrame } from './debug.js';
 
 export class DOMTemplateError extends DOMException {
-  readonly node: Node;
-
   constructor(node: Node, message: string) {
     DEBUG: {
       message += '\n' + generateNodeFrame(node);
     }
     super(message);
-    this.node = node;
   }
 }
