fac92cc5862f9ad20370e7e165024c0b77681791
Shota Nozaki <emonkak@gmail.com>
2026-06-04 22:52:36 +0900

    refactor: remove `node` and `view` properties from errors

--- src/error.ts
+++ src/error.ts
@@ -2,13 +2,10 @@ import type { View } from './core.js';
 import { formatComponentStack, getComponentStack } from './debug.js';
 
 export class RenderError extends Error {
-  readonly view: View;
-
   constructor(view: View, message?: string, options?: ErrorOptions) {
     DEBUG: {
       message += '\n' + formatComponentStack(getComponentStack(view));
     }
     super(message, options);
-    this.view = view;
   }
 }
