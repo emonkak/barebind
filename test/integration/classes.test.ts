2861b9d2dd5e39790d28c0a13e942b5613c121c8
Shota Nozaki <emonkak@gmail.com>
2026-06-18 21:43:32 +0900

    feat(dom): support array values for class attributes

--- test/integration/classes.test.ts
+++ test/integration/classes.test.ts
@@ -17,7 +17,7 @@ describe('classes', () => {
     document.body.removeChild(container);
   });
 
-  it('updates classes as a string', async () => {
+  it('updates classes with a string', async () => {
     const render = (value: unknown) => html`<div class=${value}></div>`;
 
     await root.render(render('foo')).finished;
@@ -29,21 +29,7 @@ describe('classes', () => {
     expect(target.classList.contains('bar')).toBe(true);
   });
 
-  it.each([
-    null,
-    undefined,
-  ])('removes the class attribute when the value is %s', async (value) => {
-    const render = (value: unknown) => html`<div class=${value}></div>`;
-
-    await root.render(render('foo')).finished;
-    const target = container.querySelector('div')!;
-    expect(target.hasAttribute('class')).toBe(true);
-
-    await root.render(render(value)).finished;
-    expect(target.hasAttribute('class')).toBe(false);
-  });
-
-  it('updates classes as an object', async () => {
+  it('updates classes with an object', async () => {
     const render = (value: Record<string, boolean>) =>
       html`<div class=${value}></div>`;
 
@@ -55,6 +41,35 @@ describe('classes', () => {
     await root.render(render({ active: false, disabled: true })).finished;
     expect(target.classList.contains('active')).toBe(false);
     expect(target.classList.contains('disabled')).toBe(true);
+  });
+
+  it('updates classes with an array', async () => {
+    const render = (values: unknown[]) => html`<div class=${values}></div>`;
+
+    await root.render(render(['foo', { bar: false }])).finished;
+    const target = container.querySelector('div')!;
+    expect(target.classList.contains('foo')).toBe(true);
+    expect(target.classList.contains('bar')).toBe(false);
+    expect(target.classList.contains('baz')).toBe(false);
+
+    await root.render(render([null, { bar: true }, 'baz'])).finished;
+    expect(target.classList.contains('foo')).toBe(false);
+    expect(target.classList.contains('bar')).toBe(true);
+    expect(target.classList.contains('baz')).toBe(true);
+  });
+
+  it.each([
+    null,
+    undefined,
+  ])('removes classes when the value is %s', async (value) => {
+    const render = (value: unknown) => html`<div class=${value}></div>`;
+
+    await root.render(render('foo')).finished;
+    const target = container.querySelector('div')!;
+    expect(target.classList.contains('foo')).toBe(true);
+
+    await root.render(render(value)).finished;
+    expect(target.classList.contains('foo')).toBe(false);
   });
 
   it('removes classes that are no longer in the object', async () => {
