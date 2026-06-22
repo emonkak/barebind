253554dd54019931f29116864a1242b42d8cd9d8
Shota Nozaki <emonkak@gmail.com>
2026-06-22 21:15:51 +0900

    feat(examples): add virtual-scroller example

---
+++ examples/virtual-scroller/src/VirtualScroller.ts
@@ -0,0 +1,310 @@
+import { createComponent, html, type Ref, type RenderContext } from 'barebind';
+import { EffectEvent, ImperativeHandle } from 'barebind/addons/hooks';
+import type { Component, VElement } from 'barebind/core';
+
+export interface VirtualScroller extends Component<VirtualScrollerProps<any>> {
+  <T>(props: VirtualScrollerProps<T>): VElement;
+}
+
+export interface VirtualScrollerProps<T> {
+  assumedItemHeight: number;
+  delay?: number;
+  getItemKey?: (item: T, index: number) => unknown;
+  initialItemIndex?: number;
+  items: T[];
+  offscreenRatio?: number;
+  ref?: Ref<VirtualScrollerHandle | null> | null | undefined;
+  renderItem: (item: T, index: number) => unknown;
+  scrollMargin?: string;
+}
+
+export interface VirtualScrollerHandle {
+  getMeasuredItems(): MeasuredItem[];
+  getVisibleElements(): Element[];
+  getVisibleRange(): Range;
+  scrollToIndex(index: number): void;
+}
+
+export interface MeasuredItem {
+  key: unknown;
+  height: number;
+}
+
+// A (half-open) range bounded inclusively below and exclusively above.
+export interface Range {
+  start: number;
+  end: number;
+}
+
+export const VirtualScroller: VirtualScroller = createComponent(
+  function VirtualScroller<T>(
+    this: RenderContext,
+    {
+      assumedItemHeight,
+      delay,
+      getItemKey = (_item, index) => index,
+      initialItemIndex = -1,
+      offscreenRatio = 1,
+      ref,
+      renderItem,
+      scrollMargin,
+      items,
+    }: VirtualScrollerProps<T>,
+  ): unknown {
+    const [visibleRange, setVisibleRange] = this.useState<Range>(() =>
+      initialItemIndex >= 0
+        ? {
+            start: initialItemIndex,
+            end: Math.min(initialItemIndex + 1, items.length),
+          }
+        : { start: 0, end: 0 },
+    );
+    const measuredItems = this.useMemo<MeasuredItem[]>(() => [], []);
+    const visibleElements = this.useMemo<Map<number, Element>>(
+      () => new Map(),
+      [],
+    );
+
+    const getItemHeight = (item: T, index: number): number => {
+      const measuredItem = measuredItems[index];
+      return measuredItem !== undefined &&
+        Object.is(measuredItem.key, getItemKey(item, index))
+        ? measuredItem.height
+        : assumedItemHeight;
+    };
+
+    const computeRangeHeight = (
+      start: number,
+      end: number = items.length,
+    ): number => {
+      let height = 0;
+      for (let i = start; i < end; i++) {
+        height += getItemHeight(items[i]!, i);
+      }
+      return height;
+    };
+
+    const computeVisibleRange = (top: number, bottom: number): Range => {
+      const size = items.length;
+      let start = 0;
+      let y = 0;
+
+      // Skip head items.
+      for (let i = start; i < size; i++) {
+        const height = getItemHeight(items[i]!, i);
+        if (y + height >= top) {
+          break;
+        }
+        start = i + 1;
+        y += height;
+      }
+
+      let end = start;
+
+      // Take tail items.
+      for (let i = start; i < size; i++) {
+        if (y > bottom) {
+          break;
+        }
+        y += getItemHeight(items[i]!, i);
+        end = i + 1;
+      }
+
+      return {
+        start,
+        end,
+      };
+    };
+
+    const intersectionObserverCallback = this.use(
+      EffectEvent((entries: IntersectionObserverEntry[]) => {
+        for (const entry of entries) {
+          if (!entry.isIntersecting && !entry.target.isConnected) {
+            continue;
+          }
+
+          const top =
+            -entry.target.parentElement!.getBoundingClientRect().top +
+            entry.rootBounds!.top;
+          const bottom = top + entry.rootBounds!.height;
+          const visibleRange = computeVisibleRange(top, bottom);
+
+          setVisibleRange(visibleRange, {
+            areStatesEqual: areRangesEqual,
+          });
+        }
+      }),
+    );
+    const resizeObserverCallback = this.use(
+      EffectEvent((entries: ResizeObserverEntry[]) => {
+        for (const entry of entries) {
+          if (!entry.target.isConnected) {
+            continue;
+          }
+
+          const index = Number(entry.target.getAttribute('aria-posinset')!) - 1;
+          const item = items[index];
+
+          if (item !== undefined) {
+            const key = getItemKey(item, index);
+            measuredItems[index] = {
+              key,
+              height: entry.contentRect.height,
+            };
+          }
+        }
+      }),
+    );
+
+    const intersectionObserver = this.useMemo(
+      () =>
+        new IntersectionObserver(intersectionObserverCallback, {
+          rootMargin: offscreenRatio * 100 + '%',
+          delay,
+        } as IntersectionObserverInit & { delay?: number }),
+      [],
+    );
+    const resizeObserver = this.useMemo(
+      () => new ResizeObserver(resizeObserverCallback),
+      [],
+    );
+
+    const spacerRef = this.useCallback((element: Element) => {
+      intersectionObserver.observe(element);
+      return () => {
+        intersectionObserver.unobserve(element);
+      };
+    }, []);
+    const itemRef = this.useCallback((element: Element) => {
+      const index = Number(element.getAttribute('aria-posinset')) - 1;
+      visibleElements.set(index, element);
+      resizeObserver.observe(element);
+      return () => {
+        visibleElements.delete(index);
+        resizeObserver.unobserve(element);
+      };
+    }, []);
+
+    this.use(
+      ImperativeHandle(ref, () => ({
+        getMeasuredItems(): MeasuredItem[] {
+          return measuredItems.slice();
+        },
+        getVisibleElements(): Element[] {
+          return visibleElements
+            .entries()
+            .toArray()
+            .sort((x, y) => x[0] - y[0])
+            .map((x) => x[1]);
+        },
+        getVisibleRange(): Range {
+          return structuredClone(visibleRange);
+        },
+        async scrollToIndex(
+          index: number,
+          options?: ScrollIntoViewOptions,
+        ): Promise<void> {
+          if (!withinRange(visibleRange, index)) {
+            intersectionObserver.disconnect();
+            await setVisibleRange({ start: index, end: index + 1 }).finished;
+          }
+          visibleElements.get(index)?.scrollIntoView(options);
+        },
+      })),
+    );
+
+    this.useEffect(() => {
+      for (let i = 0, l = items.length; i < l; i++) {
+        const measuredItem = measuredItems[i];
+        const key = getItemKey(items[i]!, i);
+
+        if (measuredItem === undefined || !Object.is(measuredItem.key, key)) {
+          measuredItems[i] = {
+            key,
+            height: assumedItemHeight,
+          };
+        }
+      }
+
+      measuredItems.length = items.length;
+    }, [items]);
+
+    this.useEffect(() => {
+      if (initialItemIndex >= 0) {
+        const element = visibleElements.get(initialItemIndex);
+        if (element !== undefined && !isInViewport(element)) {
+          element.scrollIntoView();
+        }
+      }
+    }, [initialItemIndex]);
+
+    const aboveSpace = computeRangeHeight(0, visibleRange.start);
+    const belowSpace = computeRangeHeight(visibleRange.end);
+
+    const aboveSpacer =
+      aboveSpace > 0
+        ? html`
+            <div
+              class="VirtualScroller-spacer"
+              style=${{ height: aboveSpace + 'px' }}
+              ${spacerRef}
+            ></div>
+          `.withKey(aboveSpace)
+        : null;
+    const belowSpacer =
+      belowSpace > 0
+        ? html`
+            <div
+              class="VirtualScroller-spacer"
+              style=${{ height: belowSpace + 'px' }}
+              ${spacerRef}
+            ></div>
+          `.withKey(belowSpace)
+        : null;
+    const renderedItems = items
+      .slice(visibleRange.start, visibleRange.end)
+      .map((item, offset) => {
+        const index = visibleRange.start + offset;
+        const key = getItemKey(item, index);
+        return html`
+          <li
+            aria-posinset=${index + 1}
+            aria-setsize=${items.length}
+            class="VirtualScroller-item"
+            ${itemRef}
+          >
+            <${renderItem(item, index)}>
+          </li>
+        `.withKey(key);
+      });
+
+    return html`
+      <div class="VirtualScroller">
+        <${aboveSpacer}>
+        <ul class="VirtualScroller-list" style=${{ scrollMargin }}>
+          <${renderedItems}>
+        </ul>
+        <${belowSpacer}>
+      </div>
+    `;
+  },
+);
+
+function areRangesEqual(x: Range, y: Range) {
+  return x.start === y.start && x.end === y.end;
+}
+
+function isInViewport(el: Element): boolean {
+  const { bottom, right, top, left } = el.getBoundingClientRect();
+
+  return (
+    bottom > 0 &&
+    right > 0 &&
+    top < window.innerHeight &&
+    left < window.innerWidth
+  );
+}
+
+function withinRange(range: Range, index: number): boolean {
+  return range.start <= index && index < range.end;
+}
