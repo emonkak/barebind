747defefd023814e90ddfdfdf8e0673719004d23
Shota Nozaki <emonkak@gmail.com>
2026-06-29 23:41:41 +0900

    feat(addons): add UpdateLogger addon for render/commit phase logging

--- examples/hackernews/src/index.ts
+++ examples/hackernews/src/index.ts
@@ -1,10 +1,13 @@
 import { DOMAdapter, DOMRoot, Runtime } from 'barebind';
+import { UpdateLogger } from 'barebind/addons/update-logger';
 
 import { App } from './App.js';
 import { AppStore } from './store.js';
 
 const runtime = new Runtime(new DOMAdapter());
 const root = new DOMRoot(document.body, runtime);
+
+runtime.use(new UpdateLogger());
 
 root.render(
   App({
--- examples/todomvc/src/index.ts
+++ examples/todomvc/src/index.ts
@@ -1,8 +1,11 @@
 import { DOMAdapter, DOMRoot, Runtime } from 'barebind';
+import { UpdateLogger } from 'barebind/addons/update-logger';
 import { App } from './App.js';
 import { TodoState, TodoStore } from './state.js';
 
 const runtime = new Runtime(new DOMAdapter());
 const root = new DOMRoot(document.body, runtime);
+
+runtime.use(new UpdateLogger());
 
 root.render(App({ store: new TodoStore(new TodoState()) }));
