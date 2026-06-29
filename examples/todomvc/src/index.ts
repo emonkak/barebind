b8cc466ab4c8ed0bd4923b0271dc6bf98c84adb3
Shota Nozaki <emonkak@gmail.com>
2026-06-30 01:21:11 +0900

    feat(examples): add UpdateLogger and UpdateProfiler plugin to almost examples

--- examples/todomvc/src/index.ts
+++ examples/todomvc/src/index.ts
@@ -1,5 +1,6 @@
 import { DOMAdapter, DOMRoot, Runtime } from 'barebind';
 import { UpdateLogger } from 'barebind/addons/update-logger';
+import { UpdateProfiler } from 'barebind/addons/update-profiler';
 import { App } from './App.js';
 import { TodoState, TodoStore } from './state.js';
 
@@ -7,5 +8,6 @@ const runtime = new Runtime(new DOMAdapter());
 const root = new DOMRoot(document.body, runtime);
 
 runtime.use(new UpdateLogger());
+runtime.use(new UpdateProfiler());
 
 root.render(App({ store: new TodoStore(new TodoState()) }));
--- examples/virtual-scroller/src/index.ts
+++ examples/virtual-scroller/src/index.ts
@@ -1,74 +1,13 @@
-import { createComponent, DOMAdapter, DOMRoot, html, Runtime } from 'barebind';
-
-import {
-  VirtualScroller,
-  type VirtualScrollerHandle,
-} from './VirtualScroller.js';
-
-interface AppProps {
-  items: { height: number; label: string }[];
-}
-
-const App = createComponent<AppProps>(function App({ items }) {
-  const [selectedIndex, setSelectedIndex] = this.useState(0);
-  const virtualScrollerHandle = this.useRef<VirtualScrollerHandle | null>(null);
-
-  const handleSelectedIndexChange = (event: Event) => {
-    const index = Number(
-      (event.target as HTMLSelectElement | HTMLInputElement).value,
-    );
-    virtualScrollerHandle.current!.scrollToIndex(index);
-    setSelectedIndex(index);
-  };
-
-  const scroller = this.useMemo(
-    () =>
-      VirtualScroller({
-        ref: virtualScrollerHandle,
-        assumedItemHeight: 200,
-        renderItem: ({ label, height }, _index) => html`
-          <div style=${{ lineHeight: height + 'px' }}>
-            ${label} (${height}px)
-          </div>
-        `,
-        items,
-      }),
-    [items],
-  );
-
-  return html`
-    <header class="Header">
-      <nav class="Header-nav">
-        <select @change=${handleSelectedIndexChange}>
-          <${items.map(
-            (item, index) => html`
-              <option
-                value=${index}
-                selected=${index === selectedIndex}
-              >
-                ${item.label}
-              </option>
-            `,
-          )}>
-        </select>
-        <input
-          type="range"
-          min=${0}
-          max=${items.length - 1}
-          $value=${selectedIndex}
-          @change=${handleSelectedIndexChange}
-        >
-      </nav>
-    </header>
-    <main class="Container">
-      <h1>Virtual Scroller</h1>
-      <${scroller}>
-    </main>
-  `;
-});
+import { DOMAdapter, DOMRoot, Runtime } from 'barebind';
+import { UpdateLogger } from 'barebind/addons/update-logger';
+import { UpdateProfiler } from 'barebind/addons/update-profiler';
+import { App } from './App.js';
 
 const runtime = new Runtime(new DOMAdapter());
 const root = new DOMRoot(document.body, runtime);
+
+runtime.use(new UpdateLogger());
+runtime.use(new UpdateProfiler());
 
 root.render(
   App({
