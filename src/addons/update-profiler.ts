3900d091f7337bc734620fbe2ca3123981f7d25c
Shota Nozaki <emonkak@gmail.com>
2026-06-30 00:19:35 +0900

    feat(addons): add UpdateProfiler for User Timing API instrumentation

---
+++ src/addons/update-profiler.ts
@@ -0,0 +1,46 @@
+import type { Commit } from '../core.js';
+import { nameOf } from '../debug.js';
+import type { Middleware, Update } from '../runtime.js';
+
+export type UserTimingAPI = Pick<Performance, 'mark' | 'measure'>;
+
+export class UpdateProfiler implements Middleware {
+  private readonly _userTiming: UserTimingAPI;
+
+  constructor(userTiming: UserTimingAPI = window.performance) {
+    this._userTiming = userTiming;
+  }
+
+  handle(update: Update, next: (update: Update) => Commit): Commit {
+    const renderStartMark = `barebind:render-start:${update.id}`;
+    const renderEndMark = `barebind:render-end:${update.id}`;
+    const commitStartMark = `barebind:commit-start:${update.id}`;
+    const commitEndMark = `barebind:commit-end:${update.id}`;
+
+    this._userTiming.mark(renderStartMark);
+    const commit = next(update);
+    this._userTiming.mark(renderEndMark);
+
+    return () => {
+      const name = nameOf(update.transaction.scope.owner);
+      this._userTiming.mark(commitStartMark);
+      commit();
+      this._userTiming.mark(commitEndMark);
+      this._userTiming.measure(
+        `Update <${name}>`,
+        renderStartMark,
+        commitEndMark,
+      );
+      this._userTiming.measure(
+        `Render <${name}>`,
+        renderStartMark,
+        renderEndMark,
+      );
+      this._userTiming.measure(
+        `Commit <${name}>`,
+        commitStartMark,
+        commitEndMark,
+      );
+    };
+  }
+}
