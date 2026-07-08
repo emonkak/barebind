921d5f1b57a9e82ec00233c6c007835cf86a6f29
Shota Nozaki <emonkak@gmail.com>
2026-07-08 23:21:57 +0900

    refactor(error): move `formatOwnerStack` from `debug.ts` into `error.ts` as internal helper

--- src/error.ts
+++ src/error.ts
@@ -1,5 +1,5 @@
-import type { Scope } from './core.js';
-import { captureOwnerStack, formatOwnerStack } from './debug.js';
+import type { Owner, Scope } from './core.js';
+import { captureOwnerStack, nameOf } from './debug.js';
 
 export class RenderError extends Error {
   constructor(scope: Scope, message?: string, options?: ErrorOptions) {
@@ -8,4 +8,16 @@ export class RenderError extends Error {
     }
     super(message, options);
   }
+}
+
+function formatOwnerStack(ownerStack: Owner[]): string {
+  const tail = ownerStack.length - 1;
+  return ownerStack
+    .map((owner, i) => {
+      const prefix = i === 0 ? '' : '   '.repeat(i - 1) + '`- ';
+      const suffix = i === tail ? ' <- ERROR occurred here!' : '';
+      const name = nameOf(owner);
+      return prefix + name + suffix;
+    })
+    .join('\n');
 }
