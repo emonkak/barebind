ec8359479060249e22c7916fccdec32cbc3c11f6
Shota Nozaki <emonkak@gmail.com>
2026-07-22 19:59:54 +0900

    test(esbuild-plugin-minify-templates): replace fixture files with inline source code in tests

--- tools/esbuild-plugin-minify-templates/test/fixtures/method-call.expected.js
+++
@@ -1,7 +0,0 @@
-// tools/esbuild-plugin-minify-templates/test/fixtures/method-call.js
-var Greet = createComponent(function Greet2(props) {
-  return Partial.html`<div class="greet">${props.greet}, <span>${props.name}</span>!</div>`;
-});
-export {
-  Greet
-};
--- tools/esbuild-plugin-minify-templates/test/fixtures/method-call.js
+++
@@ -1,9 +0,0 @@
-export const Greet = createComponent(function Greet(props) {
-  return Partial.html`
-    <div
-      class="greet"
-    >
-      ${props.greet}, <span>${props.name}</span>!
-    </div>
-  `;
-});
